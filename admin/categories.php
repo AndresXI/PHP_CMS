c86f36fbd11b615f672089b9b83a2bd278295c82
Andres Alcocer <andresalcocer7@yahoo.com>
2017-12-05 13:10:32 -0500

    added Category page

---
+++ admin/categories.php
@@ -0,0 +1,68 @@
+<?php include "includes/header.php"; ?>
+
+    <div id="wrapper">
+
+      <!-- Navigation -->
+      <?php include "includes/navigation.php"; ?>
+
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                            Welcome Admin
+                            <small>Author</small>
+                        </h1>
+
+                        <div class="col-xs-6">
+                          <form action="" method="post">
+                            <div class="form-group">
+                              <label for="cat_title">Add Category</label>
+                              <input type="text" name="cat_title" class='form-control'>
+                            </div>
+                            <div class="form-group">
+                              <input type="submit" name="submit" value="Add Category" class='btn btn-primary'>
+                            </div>
+                          </form>
+                       </div><!--Add Category Form-->
+
+                       <div class='col-xs-6'>
+
+                         <table class='table table-bordered table-hover'>
+                           <thead>
+                             <tr>
+                               <th>Id</th>
+                               <th>Category Title</th>
+                             </tr>
+                           </thead>
+                           <tbody>
+                             <tr>
+                               <td>Baseball</td>
+                               <td>Soccer</td>
+                             </tr>
+                             <tr>
+                               <td>Baseball</td>
+                               <td>Soccer</td>
+                             </tr>
+                           </tbody>
+                         </table>
+
+
+                       </div>
+
+
+
+
+                    </div>
+                </div>
+                <!-- /.row -->
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- /#page-wrapper -->
+        <?php include "includes/footer.php"; ?>
