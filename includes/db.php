c7c0cba2bcbd6767add98541ecd25ac2e2ecee18
Andres Alcocer <andresalcocer7@yahoo.com>
2018-01-23 23:07:54 -0500

    db

--- includes/db.php
+++ includes/db.php
@@ -1,5 +1,5 @@
 <?php
-// //connecting to the database
+//connecting to the database
 //
 // $db["db_host"] = "localhost";
 // $db["db_user"] = "root";
@@ -13,21 +13,21 @@
 //
 //
 // $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-//
-// // if($connection) {
-// //   echo "We are connected";
-// // }
 
+// if($connection) {
+//   echo "We are connected";
+// }
 
 
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-$server = $url["us-cdbr-iron-east-05.cleardb.ne"];
+$server = $url["us-cdbr-iron-east-05.cleardb.net"];
 $username = $url["b4528cfdbfca45"];
 $password = $url["f1bbd1d9"];
 $db = substr($url["heroku_d52aa83a8f80e95"], 1);
 
-$connection = mysqli_connect($server, $username, $password, $db);
+$connection = new mysqli($server, $username, $password, $db);
+
 
 
 
