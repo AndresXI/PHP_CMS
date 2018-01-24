cc39981e0a163cc0d833c74c23693c81e3bc56f3
Andres Alcocer <andresalcocer7@yahoo.com>
2018-01-23 23:15:34 -0500

    db

--- includes/db.php
+++ includes/db.php
@@ -1,35 +1,37 @@
 <?php
 //connecting to the database
+
+$db["db_host"] = "us-cdbr-iron-east-05.cleardb.net";
+$db["db_user"] = "b4528cfdbfca45";
+$db["db_pass"] = "f1bbd1d9";
+$db["db_name"] = "heroku_d52aa83a8f80e95";
+
+//create the value of the array into constants
+foreach($db as $key => $value) {
+  define(strtoupper($key), $value);
+}
+
+
+$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+if($connection) {
+  echo "We are connected";
+} else {
+  die("ERROR" . mysqli_error($connection)); 
+}
+
 //
-// $db["db_host"] = "localhost";
-// $db["db_user"] = "root";
-// $db["db_pass"] = "root";
-// $db["db_name"] = "CMS";
+// $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 //
-// //create the value of the array into constants
-// foreach($db as $key => $value) {
-//   define(strtoupper($key), $value);
-// }
+// $server = $url["us-cdbr-iron-east-05.cleardb.net"];
+// $username = $url["b4528cfdbfca45"];
+// $password = $url["f1bbd1d9"];
+// $db = substr($url["heroku_d52aa83a8f80e95"], 1);
+//
+// $connection = new mysqli($server, $username, $password, $db);
 //
 //
-// $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-
-// if($connection) {
-//   echo "We are connected";
-// }
-
-
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-
-$server = $url["us-cdbr-iron-east-05.cleardb.net"];
-$username = $url["b4528cfdbfca45"];
-$password = $url["f1bbd1d9"];
-$db = substr($url["heroku_d52aa83a8f80e95"], 1);
-
-$connection = new mysqli($server, $username, $password, $db);
-
-
-
-
+//
+//
 
 ?>
